359e5835160cde3cca449cbde1291c5ab0b51606
Lucas Anacleto <lucasanacleto04@gmail.com>
2025-10-23 16:34:14 -0300

    Protegendo páginas e definindo o tempo de expiração da sessão

---
+++ src/proxy.ts
@@ -0,0 +1,23 @@
+import { redirect } from "next/navigation";
+import { auth } from "./lib/auth";
+import { NextResponse } from "next/server";
+
+export default auth((request) => {
+  const isLogged = !!request.auth;
+
+  const { pathname } = request.nextUrl;
+
+  const isPrivatePath = pathname.startsWith("/dash");
+
+  if (isLogged && !isPrivatePath) {
+    return NextResponse.redirect(new URL("/dash", request.nextUrl));
+  }
+
+  if (!isLogged && isPrivatePath) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+});
+
+export const config = {
+  matcher: ["/login", "/register", "/dash", "/dash/:path"],
+};
